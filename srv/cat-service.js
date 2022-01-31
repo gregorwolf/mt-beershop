429c3d93ed24b8a034e0eaba1a0adce4ad790f44
Gregor Wolf <gregor.wolf@gmail.com>
2022-01-31 18:51:54 +0100

    try getDestinationFromDestinationService

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,11 +1,21 @@
 const cds = require("@sap/cds");
 const {
   executeHttpRequest,
+  getDestinationFromDestinationService,
   retrieveJwt,
   verifyJwt,
 } = require("@sap-cloud-sdk/core");
 const { getDestinationNameAndJwt } = require("./lib/connection-helper");
 const { getOrganizations } = require("./cf-api");
+
+async function decodeJwt(req) {
+  let jwt = retrieveJwt(req);
+  if (jwt) {
+    return await verifyJwt(jwt);
+  } else {
+    return false;
+  }
+}
 
 module.exports = cds.service.impl(async function () {
   this.on("READ", "Suppliers", async (req) => {
@@ -13,6 +23,27 @@ module.exports = cds.service.impl(async function () {
     return bupa.run(req.query);
   });
   this.on("READ", "SdkSuppliers", async (req) => {
+    let decodedJwt = await decodeJwt(req);
+    if (decodedJwt) {
+      console.log("decodedJwt: ", decodedJwt);
+      const options = {
+        iss: decodedJwt.iss,
+      };
+      const destination = await getDestinationFromDestinationService(
+        "S4HANA",
+        options
+      );
+      const response = await executeHttpRequest(destination, {
+        method: "get",
+        url: "/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Suppliers?$top=2&$select=Id,Name",
+      });
+      return response.data.d.results;
+    } else {
+      req.error({ code: 404, msg: "No valid JWT provided" });
+    }
+  });
+
+  this.on("READ", "SdkJwtSuppliers", async (req) => {
     const destinationNameAndJwt = getDestinationNameAndJwt(req, "S4HANA");
     console.log("destinationNameAndJwt: ", destinationNameAndJwt);
     const response = await executeHttpRequest(destinationNameAndJwt, {
@@ -27,9 +58,8 @@ module.exports = cds.service.impl(async function () {
       username: req.user.id,
       is_admin: req.user.is("admin"),
     };
-    let jwt = retrieveJwt(req);
-    if (jwt) {
-      let decodedJwt = await verifyJwt(jwt);
+    let decodedJwt = await decodeJwt(req);
+    if (decodedJwt) {
       user = {
         ...user,
         email: decodedJwt.email,

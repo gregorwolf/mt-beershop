d857888f8dfad40b3f23dd434d8a771a3fc331d1
Gregor Wolf <gregor.wolf@gmail.com>
2022-02-01 22:54:02 +0100

    app to maintain beers

---
+++ app/beers/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function (AppComponent) {
+  "use strict";
+
+  return AppComponent.extend("mt.beershop.beers.Component", {
+    metadata: {
+      manifest: "json",
+    },
+  });
+});
